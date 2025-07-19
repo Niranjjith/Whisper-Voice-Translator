053453227c6249b2bfd0f8388330b15a4c6446c6
Niranjan A R <niranjjithbathery@gmail.com>
2025-07-19 11:16:21 +0530

    Updated Some Features

--- transcriber/views.py
+++ transcriber/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from faster_whisper import WhisperModel
 
-# Load model once at startup
-model = WhisperModel("base", device="cpu", compute_type="int8")  # Use int8 for better CPU performance
+# ----------------Load model once at startup--------------------------
 
+model = WhisperModel("base", device="cpu", compute_type="int8")  
 def live_transcribe_view(request):
     return render(request, "transcriber/live.html")
 
@@ -15,14 +15,17 @@ def transcribe_chunk(request):
     if request.method == "POST" and request.FILES.get("audio"):
         audio_file = request.FILES["audio"]
 
-        # Save uploaded audio to temp file
+# --------------------Save uploaded audio to temp file----------------
+
         with tempfile.NamedTemporaryFile(delete=False, suffix=".webm") as temp_audio:
             for chunk in audio_file.chunks():
                 temp_audio.write(chunk)
             temp_audio_path = temp_audio.name
 
         try:
-            # Transcribe using faster-whisper
+
+ # --------------------Transcribe using faster-whisper----------------
+
             segments, _ = model.transcribe(temp_audio_path)
             text = " ".join([segment.text for segment in segments])
             return JsonResponse({"text": text})
